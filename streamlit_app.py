8edf9b1cf59c93ccd7ecc4f2e5e7d3bb6e5fa6ac
Hamas-E <58503065+Hamas-E@users.noreply.github.com>
2026-02-03 23:26:21 +0500

    Update streamlit_app.py
    Alterations to API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + "!", icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=moothiefroot_response.json(), use_container_width = True)
 
